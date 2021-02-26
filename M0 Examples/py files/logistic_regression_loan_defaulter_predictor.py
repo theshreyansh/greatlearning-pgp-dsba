48e20ed0636d6bf3998006ed1cfbeae9dc792fd6
Shreyansh <vaghela.shreyansh@gmail.com>
2021-02-26 15:05:47 +0530

    ADD Logit-Regression-Example

--- M0 Examples/py files/logistic_regression_loan_defaulter_predictor.py
+++ M0 Examples/py files/logistic_regression_loan_defaulter_predictor.py
@@ -1,3 +1,7 @@
+'''
+LOAN DATASET
+'''
+
 # required libraries
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
@@ -7,7 +11,7 @@ from sklearn.metrics import accuracy_score
 
 
 # read the dataset
-data = pd.read_csv('D:\Documents\Github\greatlearning-pgp-dsba\M0 Examples\data files\iris.csv')
+data = pd.read_csv('D:\Documents\Github\greatlearning-pgp-dsba\M0 Examples\data files\loan_data.csv')
 print(data.head())
 
 print('\n\nColumn Names\n\n')
@@ -15,22 +19,30 @@ print(data.columns)
 
 #label encode the target variable
 encode = LabelEncoder()
-data.species = encode.fit_transform(data.species)
+data.Loan_Status = encode.fit_transform(data.Loan_Status)
 
-print(data.head())
+# drop the null values
+data.dropna(how='any',inplace=True)
+
 
 # train-test-split   
 train , test = train_test_split(data,test_size=0.2,random_state=0)
 
-print('shape of training data : ',train.shape)
-print('shape of testing data',test.shape)
+
 
 # seperate the target and independent variable
-train_x = train.drop(columns=['species'],axis=1)
-train_y = train['species']
+train_x = train.drop(columns=['Loan_ID','Loan_Status'],axis=1)
+train_y = train['Loan_Status']
 
-test_x = test.drop(columns=['species'],axis=1)
-test_y = test['species']
+test_x = test.drop(columns=['Loan_ID','Loan_Status'],axis=1)
+test_y = test['Loan_Status']
+
+# encode the data
+train_x = pd.get_dummies(train_x)
+test_x  = pd.get_dummies(test_x)
+
+print('shape of training data : ',train_x.shape)
+print('shape of testing data : ',test_x.shape)
 
 # create the object of the model
 model = LogisticRegression()
@@ -39,7 +51,7 @@ model.fit(train_x,train_y)
 
 predict = model.predict(test_x)
 
-print('Predicted Values on Test Data',encode.inverse_transform(predict))
+print('Predicted Values on Test Data',predict)
 
 print('\n\nAccuracy Score on test data : \n\n')
 print(accuracy_score(test_y,predict))
